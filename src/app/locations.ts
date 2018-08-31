bd199e1f5ea5f56fa65816b3182259abf0da7597
Tim <contact@tim-media.com>
2018-08-31 14:11:47 +0200

    Location Spanischbrötlibahn updated

--- src/app/locations.ts
+++ src/app/locations.ts
@@ -6,7 +6,7 @@ export const Locations = [
     { lat: 47.403191, lng: 8.401410, title: 'Stadtbibliothek', code: '203', id: '1dlFfWXJot7iMF4fbVi3pbYDGjLFjuZ0j' },
     { lat: 47.405685, lng: 8.400687, title: 'Krone', code: '342', id: '1j_n3EVy779qRBKx7NivMhVVVsTUrDsl9' },
     { lat: 47.405121, lng: 8.404902, title: 'Römer', code: '304', id: '1CxM5VgCpvPgigGoRPeSNcq5JbEiqqm1a' },
-    { lat: 47.405832, lng: 8.405778, title: 'Spanischbrötlibahn', code: '123', id: '1JEn1pXCeTx_jp2TTLWyhQTRB9QedfYrD' },
+    { lat: 47.407690, lng: 8.404944, title: 'Spanischbrötlibahn', code: '123', id: '1JEn1pXCeTx_jp2TTLWyhQTRB9QedfYrD' },
     { lat: 47.405658, lng: 8.399680, title: 'Henkersgasse', code: '410', id: '1JKvdFFC4ycpXiodVOXpDioBs9BykjjM9' },
     { lat: 47.401588, lng: 8.403312, title: 'Fabrikglocke', code: '231', id: '1Y4VelidIcGwpOJiB94CjLqnMh9o8DPFs' }
 ]
